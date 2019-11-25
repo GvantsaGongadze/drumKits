3bfeb11b4edab232dd3cc108a6ebfe5bbe0a8cc5
Gvantsa <gvantsa.gongadze10@geolab.edu.ge>
2019-11-25 22:54:01 +0400

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('keypress', (event) => {
+
+    for (i = 97; i <=122; i++){
+        if (event.keyCode == i) {
+            let audio = document.getElementById(i);
+            
+            if(audio) {
+                audio.play(); 
+                let play = audio.parentElement;
+                play.setAttribute("id", "play")
+                
+                setTimeout(function(){
+                    play.removeAttribute("id");
+                }, 500);
+            }
+
+            
+        }
+    }
+});
+
+
